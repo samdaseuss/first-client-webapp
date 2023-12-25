35766300e1c33b105118abb2e3b8e060f93b3504
yellpall <ohj3423@naver.com>
2023-12-25 13:31:23 +0900

    변경1 : 코드 개선 - 개선 사항 1-2에 해당
    관련 이슈
    - close #1
    - 참고 #1, #2

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+    
+    return JSON.parse(ajax.response);
+}
 
-const newsFeed = JSON.parse(ajax.response);
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function() {
     const id = location.hash.substring(1);
 
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title;
