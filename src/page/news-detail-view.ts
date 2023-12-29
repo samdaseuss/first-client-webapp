124690b75ea1a98cec2d7b29fb907982573bfa93
yellpall <ohj3423@naver.com>
2023-12-29 10:30:07 +0900

    ⑧ XHR to Fetch & Promise 말고 콜백으로 작성 완료
    관련 #16, #8, #2

--- src/page/news-detail-view.ts
+++ src/page/news-detail-view.ts
@@ -1,34 +1,34 @@
 import View from '../core/view';
 import { NewsDetailApi } from '../core/api';
-import { NewsComment, NewsStore } from '../types';
+import { NewsComment, NewsStore, NewsDetail } from '../types';
 import { CONTENT_URL } from '../config';
 
 const template = `
-<div class="bg-gray-600 min-h-screen pb-8">
-  <div class="bg-white text-xl">
-    <div class="mx-auto px-4">
-      <div class="flex justify-between items-center py-6">
-        <div class="flex justify-start">
-          <h1 class="font-extrabold">Hacker News</h1>
-        </div>
-        <div class="items-center justify-end">
-          <a href="#/page/{{__currentPage__}}" class="text-gray-500">
-            <i class="fa fa-times"></i>
-          </a>
+  <div class="bg-gray-600 min-h-screen pb-8">
+    <div class="bg-white text-xl">
+      <div class="mx-auto px-4">
+        <div class="flex justify-between items-center py-6">
+          <div class="flex justify-start">
+            <h1 class="font-extrabold">Hacker News</h1>
+          </div>
+          <div class="items-center justify-end">
+            <a href="#/page/{{__currentPage__}}" class="text-gray-500">
+              <i class="fa fa-times"></i>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="h-full border rounded-xl bg-white m-6 p-4 ">
-    <h2>{{__title__}}</h2>
-    <div class="text-gray-400 h-20">
-      {{__content__}}
+    <div class="h-full border rounded-xl bg-white m-6 p-4 ">
+      <h2>{{__title__}}</h2>
+      <div class="text-gray-400 h-20">
+        {{__content__}}
+      </div>
+      {{__comments__}}
     </div>
-    {{__comments__}}
   </div>
-</div>
-`;
+  `;
 
 export default class NewsDetailView extends View {
   private store: NewsStore;
@@ -40,15 +40,17 @@ export default class NewsDetailView extends View {
 
   render = (id: string): void => {
     const api = new NewsDetailApi(CONTENT_URL.replace('@id', id));
-    const { title, content, comments } = api.getData();
+    api.getData((data: NewsDetail) => {
+      const { title, content, comments } = data;
 
-    this.store.makeRead(Number(id));
-    this.setTemplateData('currentPage', this.store.currentPage.toString());
-    this.setTemplateData('title', title);
-    this.setTemplateData('content', content);
-    this.setTemplateData('comments', this.makeComment(comments));
+      this.store.makeRead(Number(id));
+      this.setTemplateData('currentPage', this.store.currentPage.toString());
+      this.setTemplateData('title', title);
+      this.setTemplateData('content', content);
+      this.setTemplateData('comments', this.makeComment(comments));
 
-    this.updateView();
+      this.updateView();
+    });
   }
 
   private makeComment(comments: NewsComment[]): string {
